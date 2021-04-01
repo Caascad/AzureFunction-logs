dfb32a6aa6f15f64a3bb88737c9d2ec066b6d831
Romain Davaze <davaze.romain@gmail.com>
2021-04-01 16:12:03 +0200

    init

---
+++ SendLogs/main.py
@@ -0,0 +1,13 @@
+from typing import List
+import json
+import logging
+
+import azure.functions as func
+
+
+def main(events: List[func.EventHubEvent]):
+    for event in events:
+        evt = json.loads(event
+        .get_body().decode('utf-8'))
+        for record in evt['records']:
+            logging.info(record['properties']['log'])
